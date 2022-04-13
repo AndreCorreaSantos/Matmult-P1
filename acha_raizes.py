5c53e7fe59a868856dc42400a0f73b07c02d8dd4
PedroPauloMorenoCamargo <pedropmc1@al.insper.edu.br>
2022-04-13 11:03:44 -0300

    a

--- acha_raizes.py
+++ acha_raizes.py
@@ -4,6 +4,8 @@ import numpy
 rang = numpy.arange(-6*math.pi+0.0001,0,0.00001)
 
 def funcao_B(x,k):
+    if k == 0:
+        return ((5*math.sin(x/2) - (3*math.sin(4*x/3))))
     if k == 1:
         return (5*math.sin((3*math.pi)/5)*math.sin(x/2)) - (3*math.sin((8*math.pi)/5)*math.sin(4*x/3))
     if k ==2:
@@ -23,6 +25,7 @@ def acha_raiz(k):
 def curva(x):
     return (5*math.cos(x)-3*math.cos(8*x/3),5*math.sin(x)-3*math.sin(8*x/3))
 
+raizes_k0 = acha_raiz(0)
 raizes_k1 = acha_raiz(1)
 raizes_k2 = acha_raiz(2)
 raizes_k3 = acha_raiz(3)
@@ -31,11 +34,17 @@ raizes_k4= acha_raiz(4)
 
 
 #para k0 u+v = pi/2 ||| raiz + 2v = pi/2 --> v = (p/2 - raiz)/2 || u = pi/2 - v
-
+uvs_k0 = []
 uvs_k1 = []
 uvs_k2 = []
 uvs_k3 = []
 uvs_k4 = []
+for raiz in raizes_k0:
+    variavel = 0
+    v = ((variavel)-raiz)/2
+    u = (variavel) - v
+    uvs_k0.append((u,v))
+    
 for raiz in raizes_k1:
     variavel = (6*math.pi)/5
     v = ((variavel)-raiz)/2
@@ -60,4 +69,4 @@ for raiz in raizes_k4:
     u = (variavel) - v
     uvs_k4.append((u,v))
 
-print(uvs_k4)
+print(uvs_k0)
